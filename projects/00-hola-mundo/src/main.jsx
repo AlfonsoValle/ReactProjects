28aff57fcd3e7752b04e2034f2f3c28c0eada515
AlfonsoValle <valleuned@gmail.com>
2023-09-04 13:09:52 +0200

    primer avance recomendados de twitter

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { App } from './App.jsx'
+import "./index.css"
+import "./App.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render("Hola Mundo")
- 
 
+
+
+
+root.render (
+<App/>
+)
+
+//Nombres de los componentes siempre en PascalCase, React no los pilla si no es así. 
