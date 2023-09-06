82eb97ae762b61ecc41e7df46e4d9b80909b0525
AlfonsoValle <valleuned@gmail.com>
2023-09-06 12:59:51 +0200

    propagación cambios y actualización reactiva

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,22 +1,28 @@
 
+import { useState } from "react"
 import "./App.css"
 import { TwitterFollowCard } from "./TwitterFollowCard.jsx"
 
 
 export function App() {
+
+    const [name, setName] = useState ("Valle_avs")
     const formatUserName = (userName) => `@${userName}`
     return (
         <section className="App">
-            <TwitterFollowCard formatUserName={formatUserName} userName="Valle_avs" >
+            <TwitterFollowCard formatUserName={formatUserName} userName={name} >
                 Alfonso 
             </TwitterFollowCard>
-            <TwitterFollowCard  userName="midudev"  >
+            <TwitterFollowCard  userName= "midudev" >
             Midu 
             </TwitterFollowCard >
             <TwitterFollowCard  userName="MoureDev">
             Moure 
             </TwitterFollowCard>
 
+<button onClick={() => setName ("Elon_Musk")}>
+Cambiar Nombre
+</button>
         </section>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react"
 
-export function TwitterFollowCard({ userName = "JhonDoe", children, isFollowing }) {
+export function TwitterFollowCard({ userName = "JhonDoe", children }) {
+
+const [isFollowing, setIsFollowing] = useState(false)
+// isFollowing es el que investiga cómo está ahora mismo (luz apagada o encendida? y el setIsFollowing es el interruptor)
+
+const handleClick = () => {
+    setIsFollowing (!isFollowing)
+}
+
+//Pilla como esté el interruptor y dale al otro lado. Ahora ponemos esta función en el botón.
+//Son estados internos, separados para cada elemento, darle a seguir a uno no implica que sigas a otro. 
+
+
     const imageSrc = `https://unavatar.io/${userName}`
     const text = isFollowing ? "Siguiendo" : "Seguir"
     const buttonClassName = isFollowing
         ? "tw-followCard-button is-following"
         : "tw-followCard-button"
+
+        
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
@@ -20,7 +34,7 @@ export function TwitterFollowCard({ userName = "JhonDoe", children, isFollowing 
                 </div>
             </header>
             <aside>
-                <button className={buttonClassName}>
+                <button className={buttonClassName} onClick = {handleClick}>
                     {text}
                 </button>
             </aside>
